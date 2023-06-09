f9641d7bb459c4eb7f1a5655ffa727bb6fa843b6
rolandsobczak <rolandsobczak@icloud.com>
2023-06-09 15:10:48 +0200

    feat: `UserFactory`

--- src/tests/factories/users.py
+++ src/tests/factories/users.py
@@ -1,13 +1,13 @@
 import factory
 from leaf.models import User
 from leaf.routers.users import get_password_hash
-from tests.factories.common import Session
+from tests.factories.common import FactoriesSession
 
 
 class UserFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = User
-        sqlalchemy_session = Session
+        sqlalchemy_session = FactoriesSession
         sqlalchemy_session_persistence = "commit"
 
     email = factory.Sequence(lambda n: f"user{n}@example.it")
