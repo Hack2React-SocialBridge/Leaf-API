f184d31d8ae7297aa3030a46a910d9a00d7e63e8
rolandsobczak <rolandsobczak@icloud.com>
2023-06-09 15:10:15 +0200

    fix: test db building strategy changed from rollback to database drop after running tests

--- src/conftest.py
+++ src/conftest.py
@@ -2,11 +2,12 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 from sqlalchemy import text
-from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy_utils import database_exists, create_database, drop_database
 
 from leaf.main import app
 from leaf.database import get_db, Base
 from tests.test_database import SQLALCHEMY_TESTING_DATABASE_URL, engine
+from tests.factories.common import FactoriesSession
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -16,25 +17,20 @@ def db_engine():
     with engine.connect() as connection:
         create_extension = text(f"CREATE EXTENSION IF NOT EXISTS postgis;")
         connection.execute(create_extension)
-
     Base.metadata.create_all(bind=engine)
+
     yield engine
+
+    drop_database(engine.url)
 
 
 @pytest.fixture(scope="session")
 def db(db_engine):
     connection = db_engine.connect()
-
-    # begin a non-ORM transaction
-    transaction = connection.begin()
-
-    # bind an individual Session to the connection
     db = Session(bind=connection)
-    # db = Session(db_engine)
 
     yield db
 
-    db.rollback()
     connection.close()
 
 
@@ -47,6 +43,6 @@ def client(db):
         yield c
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="session", autouse=True)
 def factory_boy_session(db_engine):
-    Session.configure(bind=engine)
+    FactoriesSession.bind = db_engine
--- src/tests/factories/common.py
+++ src/tests/factories/common.py
@@ -1,4 +1,4 @@
 from sqlalchemy.orm import scoped_session, sessionmaker
 from leaf.database import engine
 
-Session = scoped_session(sessionmaker(autoflush=False, bind=engine))
+FactoriesSession = scoped_session(sessionmaker(autoflush=False, bind=engine))
