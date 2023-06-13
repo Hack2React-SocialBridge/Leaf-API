ffacc7c2021b5756a264b1d070d0a763a9b045b5
rolandsobczak <rolandsobczak@icloud.com>
2023-06-13 22:30:09 +0200

    feat: `update_one` function test

--- src/tests/test_repositories/test_users.py
+++ src/tests/test_repositories/test_users.py
@@ -1,4 +1,4 @@
-from leaf.repositories.users import get_user_by_email, get_active_user_by_email
+from leaf.repositories.users import get_user_by_email, get_active_user_by_email, create_one, update_one
 from tests.factories.users import UserFactory
 from leaf.models import User
 
@@ -27,3 +27,10 @@ def test_create_one(db):
     create_one(db, email="create_one_test@test.com", hashed_password="test", first_name="test", last_name="test", disabled=True)
     db_user = get_user_by_email(db, email="create_one_test@test.com")
     assert db_user is not None
+
+
+def test_update_one(db):
+    user = UserFactory.create(first_name="before_update")
+    update_one(db, user.email, first_name="after_update")
+    db_user = get_user_by_email(db, user.email)
+    assert db_user.first_name == "after_update"
