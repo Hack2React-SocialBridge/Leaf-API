55de23b410f89cdddb275a4434ca1075f0b852ae
rolandsobczak <rolandsobczak@icloud.com>
2023-06-22 17:21:20 +0200

    feat: Threat model

---
+++ src/leaf/models/threat.py
@@ -0,0 +1,22 @@
+from geoalchemy2 import Geometry
+from sqlalchemy import Column, ForeignKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from leaf.config.database import Base
+
+
+class Threat(Base):
+    __tablename__ = "threats"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        autoincrement=True,
+        primary_key=True,
+        index=True,
+    )
+    location = Column(Geometry("POINT"))
+    category_id: Mapped[int] = mapped_column(ForeignKey("threat_categories.id"))
+    category: Mapped["ThreatCategory"] = relationship(
+        back_populates="threats",
+        uselist=False,
+    )
