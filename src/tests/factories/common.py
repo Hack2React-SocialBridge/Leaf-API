1e6b9c35126a8d480fdfc84fbe77b8b2334815c9
rolandsobczak <rolandsobczak@icloud.com>
2023-06-23 18:00:45 +0200

    fix: invalid import in `factories/common.py`

--- src/tests/factories/common.py
+++ src/tests/factories/common.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from leaf.config import engine
+from leaf.config.database import engine
 
 FactoriesSession = scoped_session(
     sessionmaker(autoflush=False, bind=engine),
