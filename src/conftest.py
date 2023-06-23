d376e6580b3c18531300465ee69ec7fe0b22101e
rolandsobczak <rolandsobczak@icloud.com>
2023-06-23 18:02:00 +0200

    fix: postgis extension creation in tests fix

--- src/conftest.py
+++ src/conftest.py
@@ -2,15 +2,15 @@ from __future__ import annotations
 
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import text
+from sqlalchemy import DDL, text
 from sqlalchemy.orm import Session
 from sqlalchemy_utils import create_database, database_exists, drop_database
 
-from leaf.config import Base
+from leaf.config.database import Base
 from leaf.dependencies import get_db
 from leaf.main import app
+from tests.database_test import SQLALCHEMY_TESTING_DATABASE_URL, engine
 from tests.factories.common import FactoriesSession
-from tests.test_database import SQLALCHEMY_TESTING_DATABASE_URL, engine
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -19,9 +19,10 @@ def db_engine():
         create_database(engine.url)
     with engine.connect() as connection:
         create_extension = text(
-            f"CREATE EXTENSION IF NOT EXISTS postgis;",
+            f"CREATE EXTENSION postgis;",
         )
         connection.execute(create_extension)
+        connection.commit()
     Base.metadata.create_all(bind=engine)
 
     yield engine
