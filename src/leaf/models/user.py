552a7d267bfd9fc7fe65fcc0f35a2309a07829d3
rolandsobczak <rolandsobczak@icloud.com>
2023-06-23 13:14:23 +0200

    feat: Groups and permissions added

--- src/leaf/models/user.py
+++ src/leaf/models/user.py
@@ -1,10 +1,22 @@
 from __future__ import annotations
 
-from sqlalchemy import String
+import enum
+from typing import Optional
+
+from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from leaf.config.database import Base
 from leaf.models.mixins import TimestampedMixin
+
+
+class PermissionsType(enum.Enum):
+    read_users = 1
+    create_content = 2
+    grant_permissions = 4
+    revoke_permissions = 8
+    read_threats = 16
+    modify_threats = 32
 
 
 class User(TimestampedMixin, Base):
@@ -24,3 +36,19 @@ class User(TimestampedMixin, Base):
     posts: Mapped["Post"] = relationship(back_populates="user")
     comments: Mapped["Comment"] = relationship(back_populates="user")
     likes: Mapped["Like"] = relationship(back_populates="user")
+    permissions: Mapped[int]
+    group_id: Mapped[Optional[int]] = mapped_column(ForeignKey("groups.id"))
+    group: Mapped["Group"] = relationship(back_populates="users", uselist=False)
+
+
+class Group(TimestampedMixin, Base):
+    __tablename__ = "groups"
+
+    id: Mapped[int] = mapped_column(
+        autoincrement=True,
+        primary_key=True,
+        index=True,
+    )
+    name: Mapped[str] = mapped_column(String(255), unique=True)
+    permissions: Mapped[int]
+    users: Mapped["User"] = relationship(back_populates="group")
