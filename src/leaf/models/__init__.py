b17847af02443002ae460cd558cc8c72a5681153
rolandsobczak <rolandsobczak@icloud.com>
2023-06-23 18:00:01 +0200

    fix: relationships fixed

---
+++ src/leaf/models/__init__.py
@@ -0,0 +1,6 @@
+from leaf.models.comments import Comment
+from leaf.models.like import Like
+from leaf.models.post import Post
+from leaf.models.threat import Threat
+from leaf.models.threat_category import ThreatCategory
+from leaf.models.user import User
